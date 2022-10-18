282915e461b798aae499f92c8eec252785fa3fa0
m-ayo <Minayomary7@gmail.com>
2022-10-18 07:23:39 -0700

    second commit

---
+++ booleans.js
@@ -0,0 +1,11 @@
+let isloggedIn = true;
+console.log(isloggedIn);
+
+let gameOver = false;
+console.log(gameOver);
+
+let isWaterWet = true;
+console.log(isWaterWet);
+
+let animal = "dog";
+console.log(animal.length);
---
+++ strings.js
@@ -0,0 +1,48 @@
+let animal = "dog";
+console.log(animal.length);
+
+//acces individual characters in a string
+
+// changing case
+console.log(animal[3]);
+let msg = 'I am king';
+console.log(msg);
+let yelMsg = msg.toLocaleUpperCase();
+console.log(yelMsg);
+
+let angry = 'LeaVe Me aLoNe';
+let lower = angry.toLowerCase();
+console.log(lower);
+
+//trim method
+let greeting = ' leave me alone please  ';
+console.log(greeting);
+let greetings = greeting.trim();
+console.log(greeting);
+
+let tvShow = 'catdog';
+let firstIndex = tvShow.indexOf('cat');
+console.log(firstIndex);
+
+//slice
+let str = 'Lokichoggio';
+console.log(str);
+
+let partOfstr = str.slice(2, 6);
+console.log(partOfstr);
+
+let annoyingLaugh = 'teehee so funny! teehee!';
+console.log(annoyingLaugh);
+
+let replacer = annoyingLaugh.replaceAll('teehee', 'haahaa');
+console.log(replacer);
+let song = "london calling";
+console.log(song);
+
+let park = 'Yellowstone';
+const index = parkindexOf(Stone);
+console.log(index);
+
+let myName = 'mary\nminayo';
+console.log(myName);
+console.log('hello\'world');
